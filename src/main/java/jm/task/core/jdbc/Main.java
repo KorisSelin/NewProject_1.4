fa567b6bff7f07a73a421f1d3c25191f4cc7716a
Alex_Martynov <vertikal2023@yandex.ru>
2023-11-14 14:44:21 +0400

    1) Убрал Вайлдкарт в импортах 2) Убрал лишние переменные тима String sql. Подал литералы на прямую в параметры. 3) Использовал для стирания таблицы TEUNCATE 4) Удалил count

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,15 +9,15 @@ import jm.task.core.jdbc.util.Util;
 public class Main {
     public static void main(String[] args) {
 
-//        UserService userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
 //        userService.createUsersTable();
 //        userService.saveUser("Чапай", "Волков", (byte) 11);
 //        userService.saveUser("Иван", "Иванович", (byte) 22);
 //        userService.saveUser("Джим", "Кери", (byte) 33);
 //        userService.saveUser("Дональд", "Трамп", (byte) 44);
-//        for(User user : userService.getAllUsers()){
-//            System.out.println(user);
-//        }
+        for(User user : userService.getAllUsers()){
+            System.out.println(user);
+        }
 //        userService.cleanUsersTable();
 //        userService.dropUsersTable();
 //
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,7 +3,13 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
+import java.sql.Connection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,21 +26,21 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
-        String sql = "CREATE TABLE " + TABLE_NAME + " (ID INT PRIMARY KEY auto_increment, NAME varchar(30), LASTNAME varchar(30), AGE INT)";
         try (Statement statement = connection.createStatement()){
-            statement.executeUpdate(sql);
+            statement.executeUpdate("CREATE TABLE " + TABLE_NAME +
+                    " (ID INT PRIMARY KEY auto_increment, " +
+                    "NAME varchar(30), " +
+                    "LASTNAME varchar(30), " +
+                    "AGE INT)");
             System.out.println("Таблица создана\n");
         } catch (SQLException e) {
             System.err.println("Ошибка в создании таблици\n");
         }
-
     }
 
     public void dropUsersTable() {
-        String sql = "DROP TABLE " + TABLE_NAME;
         try (Statement statement = connection.createStatement()){
-            statement.executeUpdate(sql);
+            statement.executeUpdate("DROP TABLE " + TABLE_NAME);
             System.out.println("Таблица удалена\n");
         } catch (SQLException e){
             System.err.println("Ошибка удаления таблицы\n");
@@ -42,13 +48,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO " + TABLE_NAME + " ( name, lastName, age ) Values ( ?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try (PreparedStatement preparedStatement = connection
+                .prepareStatement("INSERT INTO " + TABLE_NAME + " ( name, lastName, age ) Values ( ?, ?, ?)")) {
             preparedStatement.setString(1,name);
             preparedStatement.setString(2,lastName);
             preparedStatement.setInt(3,age);
             count += preparedStatement.executeUpdate();
-
 
             System.out.printf("User %s создан.\n", name);
         } catch ( SQLException e){
@@ -57,9 +62,9 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        String sql = "DELETE FROM " + TABLE_NAME + " where id = " + id;
-        try ( PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-            preparedStatement.execute(sql);
+        try ( PreparedStatement preparedStatement = connection
+                .prepareStatement("DELETE FROM " + TABLE_NAME + " where id = " + id)){
+            preparedStatement.execute("DELETE FROM " + TABLE_NAME + " where id = " + id);
         System.out.printf(" Строка с id %d удалена \n",id);
 
         } catch (SQLException e){
@@ -69,10 +74,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         ResultSet resultSet;
-        String sql = "SELECT * FROM " + TABLE_NAME ;
         List <User> usersList = new ArrayList();
         try (Statement statement = connection.createStatement()){
-            resultSet = statement.executeQuery(sql);
+            resultSet = statement.executeQuery("SELECT * FROM " + TABLE_NAME);
             while (resultSet.next()){
                 User user = new User(resultSet.getString("name"),
                     resultSet.getString("LastName"),
@@ -87,13 +91,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        String sql = "DELETE FROM " + TABLE_NAME ;
         try ( Statement statement = connection.createStatement()){
-            statement.executeUpdate(sql);
+            statement.executeUpdate("TRUNCATE TABLE " + TABLE_NAME);
             System.out.printf("Таблица %s очищена \n", TABLE_NAME);
         } catch (SQLException e){
             System.err.printf("Ошибка очистки таблицы %s \n", TABLE_NAME);
         }
-
     }
 }
